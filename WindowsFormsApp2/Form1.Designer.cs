97afe14653b1a974fb70a503be73192450035423
Unbrocken <stas.chuhrai@gmail.com>
2020-09-17 19:35:15 +0300

    Save, load, lapki

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,28 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(701, 141);
+            this.button1.Location = new System.Drawing.Point(895, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(701, 230);
+            this.button2.Location = new System.Drawing.Point(895, 250);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 29);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Save";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -61,19 +65,43 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(683, 426);
+            this.dataGridView1.Size = new System.Drawing.Size(877, 426);
             this.dataGridView1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(895, 412);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Import";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(896, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(982, 453);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Excel";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -84,6 +112,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -39,6 +40,58 @@ namespace WindowsFormsApp2
                 this.dataGridView1.Rows.Add();
             }
         }
+
+        public void Math(string temp, int i, int j)
+        {
+            int t = 0;
+            Regex regex = new Regex(@"\w{1}\d{1}");
+            if(temp.StartsWith("="))
+            {
+                if(temp.Length==3)
+                {
+                    temp = temp.Remove(0,1);
+                    MatchCollection matches = regex.Matches(temp);
+                    if(matches.Count > 0)
+                    {
+                        switch (temp[0])
+                        {
+                            case 'A':
+                                t = 0;
+                                break;
+                            case 'B':
+                                t = 1;
+                                break;
+                            case 'C':
+                                t = 2;
+                                break;
+                            case 'D':
+                                t = 3;
+                                break;
+                            case 'E':
+                                t = 4;
+                                break;
+                            case 'F':
+                                t = 5;
+                                break;
+                            default:
+                                break;
+                        }
+                        //Console.WriteLine( dataGridView1[t,Convert.ToInt32(temp[1])].Value);
+                    }
+                }
+            }
+            else if(temp.StartsWith("\'"))
+            {
+                temp = temp.Trim('\'');
+                dataGridView1[i, j].Value = temp;
+            }
+        }
+        public void Import(object i, object j, object data)
+        {
+            int I = Convert.ToInt32(i);
+            int J = Convert.ToInt32(j);
+            dataGridView1[I, J].Value = data;
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             string connect = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\UnbrockenBackBone\source\repos\WindowsFormsApp2\WindowsFormsApp2\Database.mdf;Integrated Security=True";
@@ -49,7 +102,7 @@ namespace WindowsFormsApp2
                 {
                     if (dataGridView1[i, j].Value != null)
                     {
-                        string sqlExpression = "INSERT INTO dbo.Excel (Row,Col,Data) VALUES ('" + dataGridView1.Columns[i] + "', '" + dataGridView1.Rows[j] + "', '" + dataGridView1[i, j].Value + "')";
+                        string sqlExpression = "INSERT INTO dbo.Excel (Row,Col,Data) VALUES ('" + i + "', '" + j + "', '" + dataGridView1[i, j].Value + "')";
 
                         using (SqlConnection connection = new SqlConnection(connect))
                         {
@@ -69,6 +122,52 @@ namespace WindowsFormsApp2
                     }
                 }
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    if (dataGridView1[i, j].Value != null)
+                    {
+                        Math(dataGridView1[i, j].Value.ToString(),i,j);
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string connect = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\UnbrockenBackBone\source\repos\WindowsFormsApp2\WindowsFormsApp2\Database.mdf;Integrated Security=True";
+
+            string sqlExpression = "SELECT * FROM Excel";
+            using (SqlConnection connection = new SqlConnection(connect))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        object row = reader.GetValue(1);
+                        object col = reader.GetValue(2);
+                        object data = reader.GetValue(3);
+                        Import(row, col, data);
+                    }
+                }
+
+                reader.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            Start();
         }
     }
 }
